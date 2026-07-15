07418a4a80287b55854a572d5a198f1ad265eb8e
TridentMortis <tridentmortis@gmail.com>
2026-07-16 00:05:18 +0700

    Add files via upload

---
+++ trident_mortis_v4_service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'trident-mortis-v4-pwa-custom';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './IMG_20250731_190635_413.webp'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
